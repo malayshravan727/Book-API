cc975db65212af2a24458d9600820808517278ac
malayshravan727 <malayshravan727@gmail.com>
2021-06-29 19:25:52 +0530

    add mongoose and db setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 // Frame work
 const express = require("express");
-
+const mongoose = require("mongoose");
 // Database
 const database = require("./database/index");
 
@@ -9,6 +10,17 @@ const shapeAI = express();
 
 // Configurations
 shapeAI.use(express.json());
+
+//Establish the database
+mongoose
+  .connect(process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+).then(() => console.log("connection established!!!"));
 
 /*
 Route           /
