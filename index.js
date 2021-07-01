cd3bcba275a94b0e8609d1dbf3e2123e70a92208
malayshravan727 <malayshravan727@gmail.com>
2021-07-01 18:34:29 +0530

    add findOneAndUpdate

--- index.js
+++ index.js
@@ -173,15 +173,27 @@ Access          PUBLIC
 Parameters      isbn
 Method          PUT
 */
-shapeAI.put("/book/update/:isbn", (req, res) => {
-  database.books.forEach((book) => {
-    if (book.ISBN === req.params.isbn) {
-      book.title = req.body.bookTitle;
-      return;
-    }
-  });
+shapeAI.put("/book/update/:isbn", async (req, res) => {
 
-  return res.json({ books: database.books });
+  const updatedBook = await BookModel.findOneAndUpdate(
+  {
+    ISBN: req.params.isbn,
+  },
+  {
+    title: req.body.bookTitle,
+  },
+  {
+    new: true,
+  }
+  );
+  //database.books.forEach((book) => {
+    //if (book.ISBN === req.params.isbn) {
+     // book.title = req.body.bookTitle;
+     // return;
+   // }
+ // });
+
+  return res.json({ books: updatedBook });
 });
 
 /*
